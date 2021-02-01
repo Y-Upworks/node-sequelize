853a9755003643a071205e2a4672e9df591f1cde
vyash5075 <vyash5075@gmail.com>
2021-02-01 22:57:36 +0530

    adding login registeration

--- controllers/authcontroller.js
+++ controllers/authcontroller.js
@@ -2,6 +2,183 @@ const twilio = require("../Twilio/twilio");
 const Domain = require("../../models/Domain");
 const MailService = require("./NodeMailer");
 
+exports.registration = async (request, response, next) => {
+  try {
+    const {
+      first_name,
+      domain,
+      last_name,
+      email,
+      password,
+      isAgent,
+      signup_type,
+    } = request.body;
+
+    if (!email || !first_name || !password) {
+      throw errorHandler.createError(1007);
+    }
+    const isEmailExist = await emailchecker(email);
+    if (!isEmailExist) {
+      const domainDetails = await Domain.getDomainProperties(domain, [
+        "id",
+        "country_id",
+        "name",
+        "domain_name",
+      ]);
+      const hashedPassword = await hasher.HashPassword(password);
+      if (isAgent) {
+        let newUser = await Agent.create({
+          mobile_verified: 0,
+          first_name: first_name,
+          last_name: last_name,
+          main_email: email,
+          password: hashedPassword,
+          home_domain: domainDetails.id,
+          signup_type: signup_type,
+          email_verified: 0,
+          country_id: domainDetails.country_id,
+        });
+
+        if (!newUser) throw errorHandler.createError(1007);
+        let verifyLink = `https://${
+          domainDetails.name
+        }/verify-email/${General.encryptData({ id: newUser.id, type: "A" })}`;
+        let Mailbody = {
+          type: "EVC",
+          verifyLink: verifyLink,
+          display_name: newUser.first_name + " " + newUser.last_name,
+          domain_url: domainDetails.name,
+          domain_name: domainDetails.domain_name,
+          domain: domain,
+          email_to: newUser.main_email,
+        };
+        await MailService.sendMail(Mailbody);
+        return apiResponder(request, response, next, true, 2022, {
+          is_agent: isAgent,
+          first_name,
+          account_id: newUser.id,
+        });
+      } else {
+        let newUser = await User.create({
+          first_name: first_name,
+          last_name: last_name,
+          email: email,
+          password: hashedPassword,
+          home_domain: domainDetails.id,
+          signup_type: signup_type,
+          mobile_verified: 0,
+          email_verified: 0,
+        });
+        if (!newUser) throw errorHandler.createError(1007);
+        let verifyLink = `https://${
+          domainDetails.name
+        }/verify-email/${General.encryptData({ id: newUser.ID, type: "U" })}`;
+        let Mailbody = {
+          type: "EVC",
+          verifyLink: verifyLink,
+          display_name: newUser.first_name + " " + newUser.last_name,
+          domain_url: domainDetails.name,
+          domain_name: domainDetails.domain_name,
+          domain: domain,
+          email_to: newUser.email,
+        };
+        await MailService.sendMail(Mailbody);
+        return apiResponder(request, response, next, true, 2022, {
+          is_agent: isAgent,
+          first_name,
+          account_id: newUser.id,
+        });
+      }
+    }
+    if (isEmailExist) throw errorHandler.createError(2040);
+  } catch (error) {
+    console.log(error);
+    return apiResponder(
+      request,
+      response,
+      next,
+      false,
+      error.statusCode || 2040,
+      {}
+    );
+  }
+};
+
+exports.login = async (request, response, next) => {
+  try {
+    const { password } = request.body;
+    const emailid = request.body.email;
+    console.log(emailid, password);
+    if (!emailid || !password) {
+      throwerrorHandler.createError(1000);
+    }
+    const user = await emailchecker(emailid);
+
+    if (user) {
+      var email;
+      var type;
+      const isAgent = await isUserorAgent(emailid);
+      console.log(isAgent);
+      if (isAgent === "A") {
+        if (user.email_verified === 0 && user.mobile_verified === 0)
+          throwerrorHandler.createError(2099);
+        else {
+          email = user.main_email;
+          type = "A";
+        }
+      } else {
+        if (user.email_verified === 0 && user.mobile_verified === 0)
+          throwerrorHandler.createError(2099);
+        else {
+          email = user.email;
+          type = "U";
+        }
+      }
+      const checked = hasher.CheckPassword(password, user.password);
+
+      if (checked) {
+        const authToken = jwt.sign(
+          { id: user.id, email: email, type: type },
+          process.env.JWT_SECRET
+        );
+
+        const {
+          id,
+          image_url,
+          membership_id,
+          membership_status,
+          display_name,
+        } = user;
+        const data = {
+          authToken,
+          email,
+          type,
+          display_name,
+          membership_status,
+          membership_id,
+          image_url,
+          id,
+        };
+        return apiResponder(request, response, next, true, 2000, {
+          data,
+        });
+      }
+      if (!checked) throwerrorHandler.createError(1001);
+    }
+    if (!user) throwerrorHandler.createError(1000);
+  } catch (error) {
+    console.log(error);
+    return apiResponder(
+      request,
+      response,
+      next,
+      false,
+      error.statusCode || 1000,
+      {}
+    );
+  }
+};
+
 exports.sendOtp = async (request, response, next) => {
   try {
     const {
